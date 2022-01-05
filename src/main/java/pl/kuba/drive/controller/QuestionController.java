2fccb64d4ea86d625c33e7d1f643a8705569e108
Jakub Brózda <brozdajakub@gmail.com>
2022-01-05 18:34:08 +0100

    Question Controller

---
+++ src/main/java/pl/kuba/drive/controller/QuestionController.java
@@ -0,0 +1,39 @@
+package pl.kuba.drive.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import pl.kuba.drive.dto.model.QuestionDTO;
+import pl.kuba.drive.service.QuestionService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/questions")
+@RequiredArgsConstructor
+public class QuestionController {
+
+    private final QuestionService questionService;
+
+    @GetMapping()
+    public List<QuestionDTO> findAll() {
+        return questionService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public QuestionDTO findById(@PathVariable Long id) {
+        return questionService.findById(id);
+    }
+
+    @PostMapping
+    public QuestionDTO create(@RequestBody QuestionDTO questionDTO) {
+        return questionService.create(questionDTO);
+    }
+
+    @DeleteMapping
+    public void delete(@PathVariable Long id) {
+        questionService.deleteById(id);
+    }
+    
+    
+    
+}
