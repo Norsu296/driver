9f00fb39a62d018b3d13551e6f3e3304e1f60c99
Jakub Brózda <brozdajakub@gmail.com>
2022-01-05 18:21:40 +0100

    Tag controller

---
+++ src/main/java/pl/kuba/drive/controller/TagController.java
@@ -0,0 +1,37 @@
+package pl.kuba.drive.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import pl.kuba.drive.dto.model.TagDTO;
+import pl.kuba.drive.service.TagService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tags")
+@RequiredArgsConstructor
+public class TagController {
+
+    private final TagService tagService;
+
+    @GetMapping()
+    public List<TagDTO> findAll() {
+        return tagService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public TagDTO findById(@PathVariable Long id) {
+        return tagService.findById(id);
+    }
+
+    @PostMapping
+    public TagDTO create(@RequestBody TagDTO tagDTO) {
+        return tagService.create(tagDTO);
+    }
+
+    @DeleteMapping
+    public void delete(@PathVariable Long id) {
+        tagService.deleteById(id);
+    }
+}
