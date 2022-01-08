3d454fa8a00bd21030d5f1ff99adf5a6a448359e
Jakub Brózda <brozdajakub@gmail.com>
2022-01-08 15:30:38 +0100

    Keycloak

---
+++ src/main/java/pl/kuba/drive/config/KeycloakSecurityConfig.java
@@ -0,0 +1,16 @@
+package pl.kuba.drive.config;
+
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakSecurityConfig {
+
+    @Bean
+    public KeycloakConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+}
--- src/main/java/pl/kuba/drive/controller/AdviceController.java
+++ src/main/java/pl/kuba/drive/controller/AdviceController.java
@@ -6,6 +6,7 @@ import pl.kuba.drive.dto.model.AdviceDTO;
 import pl.kuba.drive.dto.model.TagDTO;
 import pl.kuba.drive.service.AdviceService;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
@@ -15,22 +16,24 @@ public class AdviceController {
 
     private final AdviceService adviceService;
 
+    @RolesAllowed("user")
     @GetMapping()
     public List<AdviceDTO> findAll() {
         return adviceService.findAll();
     }
 
+    @RolesAllowed("admin")
     @GetMapping("/{id}")
     public AdviceDTO findById(@PathVariable Long id) {
         return adviceService.findById(id);
     }
 
-    @GetMapping("/like/add/{id}")
+    @PatchMapping("/like/add/{id}")
     public AdviceDTO addLike(@PathVariable Long id) {
         return adviceService.like(id, "add");
     }
 
-    @GetMapping("/like/remove/{id}")
+    @PatchMapping("/like/remove/{id}")
     public AdviceDTO removeLike(@PathVariable Long id) {
         return adviceService.like(id, "remove");
     }
