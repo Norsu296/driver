6861d010776d3189b22e3178d8103b0b4477d493
Jakub Brózda <brozdajakub@gmail.com>
2022-01-24 18:29:54 +0100

    fix

--- src/main/java/pl/kuba/drive/DriveApplication.java
+++ src/main/java/pl/kuba/drive/DriveApplication.java
@@ -3,10 +3,12 @@ package pl.kuba.drive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import pl.kuba.drive.config.properties.ApiProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ApiProperties.class)
+@EnableJpaAuditing
 public class DriveApplication {
 
     public static void main(String[] args) {
